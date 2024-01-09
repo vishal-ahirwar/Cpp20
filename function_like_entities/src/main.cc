98080ebfaed4ad1e3466a96f8e274cf5da59837c
vishal-ahirwar <vishalchoudhry0091@gmail.com>
2024-01-09 17:04:21 +0530

    simple calculator but complicated code

--- function_like_entities/src/main.cc
+++ function_like_entities/src/main.cc
@@ -1,26 +1,8 @@
 
 // Auto Genrated C++ file by newton CLI
 //Simple Calculator but complicated code ;)
-#include <iostream>
-
-void onScreen(void (*printFuction)(const std::string &), const std::string &str)
-{
-    printFuction(str);
-};
-void print(const std::string &str)
-{
-    std::cout << "[print] >> " << str << std::endl;
-};
-
-int add(int a, int b) { return a + b; };
-int sub(int a, int b) { return a - b; };
-int multy(int a, int b) { return a * b; };
-int divide(int a, int b)
-{
-    if (b == 0)return 0;
-    else return a / b;
-};
-
+#include"utility.hxx"
+#include<string.h>
 int main(int argc, char *argv[])
 {
 
@@ -28,10 +10,10 @@ int main(int argc, char *argv[])
     int a{}, b{}, input{};
 
     onScreen(print, "Enter Two values?");
-    std::cin >> a >> b;
+    scanf("%d %d",&a,&b);
 
-    onScreen(print, "1.add,2.sub,3.multy,4.divide\n");
-    std::cin >> input;
+    onScreen(print, "1.add,2.sub,3.multy,4.divide");
+    scanf("%d",&input);
 
     switch (input)
     {
@@ -41,7 +23,7 @@ int main(int argc, char *argv[])
     case 4:{excute = divide;break;};
     default:{excute = nullptr;}
     };
-    if (excute)std::cout << excute(a, b) << std::endl;
-    else std::cout << "Invalid action!" << std::endl;
+    if (excute)onScreen(printd,excute(a,b)) ;
+    else onScreen(print,"Invalid Action");
     return 0;
 };
---
+++ function_like_entities/src/utility.hxx
@@ -0,0 +1,31 @@
+#ifndef _UTILITY_
+#define _UTILITY_
+#include<stdio.h>
+
+void onScreen(void (*printFuction)(const char*), const char *str)
+{
+    printFuction(str);
+};
+void onScreen(void (*printFunction)(int),int c)
+{
+    printFunction(c);
+}
+void printd(int c)
+{
+    printf("[%d]\n",c);
+}
+void print(const char*str)
+{
+    printf("[%s]\n",str);
+};
+
+int add(int a, int b) { return a + b; };
+int sub(int a, int b) { return a - b; };
+int multy(int a, int b) { return a * b; };
+int divide(int a, int b)
+{
+    if (b == 0)return 0;
+    else return a / b;
+};
+
+#endif
